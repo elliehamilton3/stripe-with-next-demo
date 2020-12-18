82c0b118c20d340b57f3af6212126cd9a20d6e4b
Ellie Hamilton <eleanor.hamilton@candide.eu>
2020-12-18 15:54:28 +0000

    Add other payment options in

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,22 @@ const IndexPage: NextPage = () => {
             </a>
             </Link>
         </li>
+        <li>
+          <Link href="/donate-with-elements">
+            <a className="card elements-style-background">
+              <h2 className="bottom">Donate with Elements</h2>
+              <img src="/elements-card-payment.svg" />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/use-shopping-cart">
+            <a className="card cart-style-background">
+              <h2 className="bottom">Use Shopping Cart</h2>
+              <img src="/use-shopping-cart.png" />
+            </a>
+          </Link>
+        </li>
       </ul>
     </Layout>
   )
